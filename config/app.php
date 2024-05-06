51e2cb472c073ae228a72abb353d85c2696af400
Sunny-Clavijo <169090128+Sunny-Clavijo@users.noreply.github.com>
2024-05-06 22:29:56 +0000

    update

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 
-const APP_URL="http://localhost/";
+const APP_URL="https://api.sunnysbakery.com.co/";
 const APP_NAME="API Sunny's Bakery";
 const APP_SESSION_NAME="API_SB";
 
--- config/server.php
+++ config/server.php
@@ -1,6 +1,6 @@
 <?php
 
-const DB_SERVER="localhost";
+const DB_SERVER="api.sunnysbakery.com.co";
 const DB_NAME="sunnysbakery";
 const DB_USER="root";
 const DB_PASS="";
