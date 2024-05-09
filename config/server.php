493eb84ad67ea5b1e97d14a20b407bd1ad538158
Sunny-Clavijo <169090128+Sunny-Clavijo@users.noreply.github.com>
2024-05-09 10:35:56 -0500

    Actualizar server.php

--- config/server.php
+++ config/server.php
@@ -1,7 +1,10 @@
 <?php
 
 const DB_SERVER="sunnysbakery.com.co";
-const DB_NAME="sunnysba_API_Usuarios";
+const DB_USERS="sunnysba_API_Usuarios";
+const DB_PRODUCTS="sunnysba_API_Productos";
+const DB_ORDERS="sunnysba_API_Ordenes";
+const DB_FINC="sunnysba_API_Finanzas";
 const DB_USER="sunnysba_ApiAdmin";
 const DB_PASS="SQL/Sunny.Ba*secret.2024";
 
